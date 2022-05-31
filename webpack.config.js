8adada5e8203ec7b69ee20ae682039d0079e9248
Withney Guilherme <wguilhermecriativo@gmail.com>
2022-05-31 17:10:19 -0300

    chore: add react fast refresh

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ReactRefreshPlugin = require('@pmmmwh/react-refresh-webpack-plugin')
 
 const isDevelopment = process.env.NODE_ENV !== 'production'
 
@@ -16,20 +17,29 @@ module.exports = {
   },
   devServer: {
     static: path.resolve(__dirname, 'public', 'index.html'),
+    hot: true,
   },
   plugins: [
+    isDevelopment && new ReactRefreshPlugin(),
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, 'public', 'index.html')
 
     })
-  ],
+  ].filter(Boolean),
 
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        use: 'babel-loader', 
+        use: {
+          loader: 'babel-loader',
+          options: {
+            plugins: [
+              isDevelopment && require.resolve('react-refresh/babel')
+            ].filter(Boolean)
+          }
+        }, 
       },
       {
         test: /\.scss$/,
